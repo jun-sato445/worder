831d6202031b157d9498c6e9ca97900822694ebc
MeetCommonSummer <kazmax041119@gmail.com>
2022-06-14 23:01:53 +0900

    6/14 card追加

--- gui/worder/src/components/Menu/menu.jsx
+++ gui/worder/src/components/Menu/menu.jsx
@@ -1,12 +1,39 @@
 import * as React from 'react';
 import Header from '../Common/header';
+
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+import Image from './curry.jpg';
+
 function menu() {
   
-  
+
   return (
     <>
     <Header />
-    <h2>Menu</h2>;
+    <h2>Menu</h2>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          Image={Image}
+          alt="カレーライス"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
     </>
     );
 }
