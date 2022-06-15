d591a32c464bd1afd38291090de849fb8162a966
jun-sato445 <jun.fms445@gmail.com>
2022-06-15 14:03:19 +0900

    ファイル整理

--- _bk/main_bk.py
+++ _bk/main_bk.py
@@ -8,9 +8,9 @@ async def read_root():
     return {"Message": "Hello World"}
 
 
-# @app.get("/items/{item_id}")
-# async def read_item(item_id: int, q: str = None):
-#     return {"item_id": item_id, "q": q}
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
 
 @app.get("/countries/{country_name}")
 async def country(country_name: int):
