24e1d9a5cd5c7cb6158bc42fb20d9a0244f2458a
MeetCommonSummer <kazmax041119@gmail.com>
2022-06-17 01:25:30 +0900

    test

--- gui/worder/src/components/Menu/menu.jsx
+++ gui/worder/src/components/Menu/menu.jsx
@@ -1,41 +1,67 @@
 import * as React from 'react';
 import Header from '../Common/header';
 
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
-import Image from './curry.jpg';
+import image from './curry.jpg'
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 function menu() {
-  
 
   return (
     <>
-    <Header />
-    <h2>Menu</h2>
-    <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="140"
-          Image={Image}
-          alt="カレーライス"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Lizard
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+      <Header />
+      <h2>Menu</h2>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={4}>
+          <Grid item xs={3}>
+            <Item>
+              <Card sx={{ maxWidth: 500 }}>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    height="140"
+                    image={image}
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      カレー
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      説明文：テストテストテスト
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Item>
+          </Grid>
+          <Grid item xs={3}>
+            <Item>xs=3</Item>
+          </Grid>
+          <Grid item xs={3}>
+            <Item>xs=3</Item>
+          </Grid>
+          <Grid item xs={3}>
+            <Item>xs=3</Item>
+          </Grid>
+        </Grid>
+      </Box>
     </>
-    );
+  );
 }
-
 export default menu;
